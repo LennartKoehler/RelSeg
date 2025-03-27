879d57794756cf49ee4a1f364ca2104cd2e3d370
zo48kij_turm <lennart.koehler@uni-jena.de>
2025-03-27 14:52:56 +0100

    removed visualization and changed requirements

--- bonito/lrp/legacy.py
+++ bonito/lrp/legacy.py
@@ -159,3 +159,67 @@ def one_hot(y, indices): # same as just taking the index
     one_hot_tensor[indices[0], indices[1], indices[2]] = 1
     encoded = torch.mul(one_hot_tensor, y)
     return encoded.sum()
+
+
+#----------------visualization--------------
+import torch
+import torchviz
+
+def visualize(model, model_shape):
+    x = torch.randn(model_shape, dtype=torch.float).to("cuda")
+    y = model(x)
+    print(y.shape)
+    dot = torchviz.make_dot(y, params=dict(model.named_parameters()), show_saved=True)
+    dot.format = 'pdf'
+    dot.render('visualization/model_arch_sup')
+
+"""
+Bonito model viewer - display a model architecture for a given config.
+"""
+
+import os.path
+
+import toml
+import argparse
+from bonito.util import load_symbol
+from visualization import visualize
+
+
+def main(args):
+    
+    if os.path.isdir(args.config):
+        config = toml.load(os.path.join(args.config, "config.toml"))
+    else:
+        config = toml.load(args.config)
+    Model = load_symbol(config, "Model")
+    model = Model(config).to("cuda")
+    # batchsize = config["basecaller"]["batchsize"]
+    batchsize = 1
+    chunksize = config["basecaller"]["chunksize"]
+    channels = 1
+    print(model)
+    print("Total parameters in model", sum(p.numel() for p in model.parameters()))
+    visualize(model, (batchsize, channels, chunksize))
+
+def test_main(config):
+    if os.path.isdir(config):
+        config = toml.load(os.path.join(config, "config.toml"))
+    else:
+        config = toml.load(config)
+    Model = load_symbol(config, "Model")
+    model = Model(config).to("cuda")
+    # batchsize = config["basecaller"]["batchsize"]
+    batchsize = 1
+    chunksize = config["basecaller"]["chunksize"]
+    channels = 1
+    print(model)
+    print("Total parameters in model", sum(p.numel() for p in model.parameters()))
+    visualize(model, (batchsize, channels, chunksize))
+
+def argparser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        add_help=False
+    )
+    parser.add_argument("config")
+    return parser
